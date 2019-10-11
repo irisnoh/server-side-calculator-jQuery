83d9109d294674841ff613fc097e2042b7d24695
Iris Noh <irisnoh22@gmail.com>
2019-10-11 16:01:33 -0500

    keep what is appended on the DOM after refresh

--- server/public/client.js
+++ server/public/client.js
@@ -1,15 +1,64 @@
-console.log ('im in javascript');
+console.log('im in javascript');
 
 $(document).ready(onReady);
 
-function onReady () {
+function onReady() {
     console.log('in jqeruy');
     $('#equal').on('click', onEqualClick)
+    $('#add').on('click', onAddClick)
+    $('#subtract').on('click', onSubtractClick)
+    $('#multiply').on('click', onMultiplyClick)
+    $('#divide').on('click', onDivideClick)
+    $('#clear').on('click', onClearClick)
+    getMathResults() // keep whats in my mathResults on DOM when refreshed
 }
 
-function onEqualClick () {
+function onEqualClick() {
     console.log('i/m totaling');
-   $('#appendMathHere').append(`
-   <li>SOMETHING</li>
-   `)
+    $.ajax({
+        type: 'POST',
+        url: '/newresults',
+        data: {
+            results: 'resultshere'
+        }
+    })
+    
+   getMathResults()
+}
+
+
+function getMathResults() {
+    $.ajax({
+        type: 'GET',
+        url: '/results'
+    }).then(function (response) {
+        $('#appendMathHere').empty();
+        for (let i = 0; i < response.length; i++) {
+            let results = response[i];
+        $('#appendMathHere').append(`
+        <li>${results}</li>
+        `)
+    }
+})
+}
+
+
+function onAddClick() {
+    console.log('i/m adding');
+}
+
+function onSubtractClick() {
+    console.log('i/m subtracting');
+} 
+
+function onMultiplyClick() {
+    console.log('i/m multiplying');
+}
+
+function onDivideClick() {
+    console.log('i/m diving');
+}
+
+function onClearClick() {
+    console.log('i/m clearing');
 }
--- server/server.js
+++ server/server.js
@@ -3,11 +3,23 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
+let mathResults = [ {
+    results:'something'
+}];
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.get('/results', (req,res) => {
+    res.send(mathResults);
+});
+
+app.post('/newresults', (req,res) => {
+    mathResults.push(req.body);
+    res.sendStatus(200);
+}
+);
 
 app.listen(PORT, () => {
     console.log(`Up and running on PORT: ${PORT}`);
